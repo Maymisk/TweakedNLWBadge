3eff163a3bee2b1e4a2151697fee8e5b3420070a
Khalil mmbohner <92688722+Maymisk@users.noreply.github.com>
2021-10-23 19:27:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const smedia = {
+  github: 'Maymisk',
+  instagram: 'maymi_sk',
+  twitter: 'maykbrito',
+  facebook: 'maykbrito'
+}
+
+function changeLinks() {
+  for (let li of shittyList.children) {
+    const social = li.getAttribute('class')
+    li.children[0].href = `https://${social}.com/${smedia[social]}/`
+  }
+}
+
+changeLinks()
+
+function getGitHubUserData() {
+  const url = `https://api.github.com/users/${smedia.github}`
+
+  fetch(url)
+    .then(reponse => reponse.json())
+    .then(data => {
+      MyName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userProfilePicture.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubUserData()
